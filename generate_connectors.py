adaf5b53acd4c087b45bf4e01c9339069f706723
Danilo Bargen <mail@dbrgn.ch>
2018-10-12 00:25:26 +0200

    generate_connectors: Refactor UUID handling

--- generate_connectors.py
+++ generate_connectors.py
@@ -43,14 +43,21 @@ def now() -> str:
     return datetime.utcnow().isoformat() + 'Z'
 
 
-def uuid(kind: str, typ: str, pin_number: int, identifier: str = None) -> str:
+def uuid(category: str, kind: str, variant: str, identifier: str) -> str:
     """
     Return a uuid for the specified pin.
+
+    Params:
+        category:
+            For example 'cmp' or 'pkg'.
+        kind:
+            For example 'pinheader' or 'pinsocket'.
+        variant:
+            For example '1x5-D1.1' or '1x13'.
+        identifier:
+            For example 'pad-1' or 'pin-13'.
     """
-    if identifier:
-        key = '{}-{}-1x{}-{}'.format(kind, typ, pin_number, identifier).lower().replace(' ', '~')
-    else:
-        key = '{}-{}-1x{}'.format(kind, typ, pin_number).lower().replace(' ', '~')
+    key = '{}-{}-{}-{}'.format(category, kind, variant, identifier).lower().replace(' ', '~')
     if key not in uuid_cache:
         uuid_cache[key] = str(uuid4())
     return uuid_cache[key]
@@ -88,31 +95,39 @@ def generate_pkg(
     min_pads: int,
     max_pads: int,
     top_offset: float,
-    generate_silkscreen: Callable[[List[str], str, int, float], None]
+    generate_silkscreen: Callable[[List[str], str, str, str, int, float], None]
 ):
+    category = 'pkg'
     for i in range(min_pads, max_pads + 1):
         for drill in pad_drills:
             lines = []
 
-            drillkind = kind + '-D{:.1f}'.format(drill)
-            pkg_uuid = uuid(drillkind, 'pkg', i)
+            variant = '1x{}-D{:.1f}'.format(i, drill)
+
+            def _uuid(identifier):
+                return uuid(category, kind, variant, identifier)
+
+            uuid_pkg = _uuid('pkg')
+            uuid_pads = [_uuid('pad-{}'.format(p)) for p in range(i)]
+            uuid_footprint = _uuid('footprint-default')
+            uuid_text_name = _uuid('text-name')
+            uuid_text_value = _uuid('text-value')
 
             # General info
-            lines.append('(librepcb_package {}'.format(pkg_uuid))
+            lines.append('(librepcb_package {}'.format(uuid_pkg))
             lines.append(' (name "{} 1x{} ⌀{:.1f}")'.format(name, i, drill))
             lines.append(' (description "A 1x{} {} with {}mm pin spacing '
                            'and {:.1f}mm drill holes.\\n\\n'
                            'Generated with {}")'.format(i, name_lower, spacing, drill, generator))
-            lines.append(' (keywords "connector, 1x{}, {}")'.format(i, keywords))
+            lines.append(' (keywords "connector, 1x{}, d{:.1f}, {}")'.format(i, drill, keywords))
             lines.append(' (author "{}")'.format(author))
             lines.append(' (version "0.1")')
             lines.append(' (created {})'.format(now()))
             lines.append(' (deprecated false)')
             lines.append(' (category {})'.format(pkgcat))
-            pad_uuids = [uuid(drillkind, 'pad', i, str(p)) for p in range(i)]
             for j in range(1, i + 1):
-                lines.append(' (pad {} (name "{}"))'.format(pad_uuids[j - 1], j))
-            lines.append(' (footprint {}'.format(uuid(drillkind, 'footprint', i, 'default')))
+                lines.append(' (pad {} (name "{}"))'.format(uuid_pads[j - 1], j))
+            lines.append(' (footprint {}'.format(uuid_footprint))
             lines.append('  (name "default")')
             lines.append('  (description "")')
 
@@ -120,27 +135,27 @@ def generate_pkg(
             for j in range(1, i + 1):
                 y = get_y(j, i, spacing)
                 shape = 'rect' if j == 1 else 'round'
-                lines.append('  (pad {} (side tht) (shape {})'.format(pad_uuids[j - 1], shape))
+                lines.append('  (pad {} (side tht) (shape {})'.format(uuid_pads[j - 1], shape))
                 lines.append('   (pos 0.0 {}) (rot 0.0) (size {} {}) (drill {})'.format(
                     y, pad_size[0], pad_size[1], drill,
                 ))
                 lines.append('  )')
 
             # Silkscreen
-            generate_silkscreen(lines, drillkind, i, top_offset)
+            generate_silkscreen(lines, category, kind, variant, i, top_offset)
 
             # Labels
             y_max, y_min = get_rectangle_bounds(i, spacing, top_offset + 1.27)
             text_attrs = '(height {}) (stroke_width 0.2) ' \
                          '(letter_spacing auto) (line_spacing auto)'.format(pkg_text_height)
-            lines.append('  (stroke_text {} (layer top_names)'.format(uuid(drillkind, 'label-name', i)))
+            lines.append('  (stroke_text {} (layer top_names)'.format(uuid_text_name))
             lines.append('   {}'.format(text_attrs))
             lines.append('   (align center bottom) (pos 0.0 {}) (rot 0.0) (auto_rotate true)'.format(
                 y_max,
             ))
             lines.append('   (mirror false) (value "{{NAME}}")')
             lines.append('  )')
-            lines.append('  (stroke_text {} (layer top_values)'.format(uuid(drillkind, 'label-value', i)))
+            lines.append('  (stroke_text {} (layer top_values)'.format(uuid_text_value))
             lines.append('   {}'.format(text_attrs))
             lines.append('   (align center top) (pos 0.0 {}) (rot 0.0) (auto_rotate true)'.format(
                 y_min,
@@ -151,7 +166,7 @@ def generate_pkg(
             lines.append(' )')
             lines.append(')')
 
-            pkg_dir_path = path.join(dirpath, pkg_uuid)
+            pkg_dir_path = path.join(dirpath, uuid_pkg)
             if not (path.exists(pkg_dir_path) and path.isdir(pkg_dir_path)):
                 makedirs(pkg_dir_path)
             with open(path.join(pkg_dir_path, '.librepcb-pkg'), 'w') as f:
@@ -160,18 +175,20 @@ def generate_pkg(
                 f.write('\n'.join(lines))
                 f.write('\n')
 
-            print('1x{} ⌀{:.1f}: Wrote package {}'.format(i, drill, pkg_uuid))
+            print('1x{} ⌀{:.1f}: Wrote package {}'.format(i, drill, uuid_pkg))
 
 
 def generate_silkscreen_female(
     lines: List[str],
+    category: str,
     kind: str,
+    variant: str,
     pin_count: int,
     top_offset: float,
 ) -> None:
-    lines.append('  (polygon {} (layer top_placement)'.format(
-        uuid(kind, 'polygon', pin_count, 'contour')
-    ))
+    uuid_polygon = uuid(category, kind, variant, 'polygon-contour')
+
+    lines.append('  (polygon {} (layer top_placement)'.format(uuid_polygon))
     lines.append('   (width {}) (fill false) (grab true)'.format(line_width))
     y_max, y_min = get_rectangle_bounds(pin_count, spacing, top_offset)
     lines.append('   (vertex (pos -1.27 {}) (angle 0.0))'.format(y_max))
@@ -184,14 +201,16 @@ def generate_silkscreen_female(
 
 def generate_silkscreen_male(
     lines: List[str],
+    category: str,
     kind: str,
+    variant: str,
     pin_count: int,
     top_offset: float,
 ) -> None:
+    uuid_polygon = uuid(category, kind, variant, 'polygon-contour')
+
     # Start in top right corner, go around the pads clockwise
-    lines.append('  (polygon {} (layer top_placement)'.format(
-        uuid(kind, 'polygon', pin_count, 'contour')
-    ))
+    lines.append('  (polygon {} (layer top_placement)'.format(uuid_polygon))
     lines.append('   (width {}) (fill false) (grab true)'.format(line_width))
     # Down on the right
     for pin in range(1, pin_count + 1):
@@ -223,13 +242,24 @@ def generate_sym(
     min_pads: int,
     max_pads: int,
 ):
+    category = 'sym'
     for i in range(min_pads, max_pads + 1):
         lines = []
 
-        sym_uuid = uuid(kind, 'sym', i)
+        variant = '1x{}'.format(i)
+
+        def _uuid(identifier):
+            return uuid(category, kind, variant, identifier)
+
+        uuid_sym = _uuid('sym')
+        uuid_pins = [_uuid('pin-{}'.format(p)) for p in range(i)]
+        uuid_polygon = _uuid('polygon-contour')
+        uuid_decoration = _uuid('polygon-decoration')
+        uuid_text_name = _uuid('text-name')
+        uuid_text_value = _uuid('text-value')
 
         # General info
-        lines.append('(librepcb_symbol {}'.format(sym_uuid))
+        lines.append('(librepcb_symbol {}'.format(uuid_sym))
         lines.append(' (name "{} 1x{}")'.format(name, i))
         lines.append(' (description "A 1x{} {}.\\n\\n'
                      'Generated with {}")'.format(i, name_lower, generator))
@@ -239,9 +269,8 @@ def generate_sym(
         lines.append(' (created {})'.format(now()))
         lines.append(' (deprecated false)')
         lines.append(' (category {})'.format(cmpcat))
-        pin_uuids = [uuid(kind, 'pin', i, str(p)) for p in range(i)]
         for j in range(1, i + 1):
-            lines.append(' (pin {} (name "{}")'.format(pin_uuids[j - 1], j))
+            lines.append(' (pin {} (name "{}")'.format(uuid_pins[j - 1], j))
             lines.append('  (position 5.08 {}) (rotation 180.0) (length 3.81)'.format(
                 get_y(j, i, spacing)
             ))
@@ -249,9 +278,7 @@ def generate_sym(
 
         # Polygons
         y_max, y_min = get_rectangle_bounds(i, spacing, spacing)
-        lines.append(' (polygon {} (layer sym_outlines)'.format(
-            uuid(kind, 'polygon', i, 'sym-outline')
-        ))
+        lines.append(' (polygon {} (layer sym_outlines)'.format(uuid_polygon))
         lines.append('  (width {}) (fill false) (grab true)'.format(line_width))
         lines.append('  (vertex (pos -{} {}) (angle 0.0))'.format(spacing, y_max))
         lines.append('  (vertex (pos {} {}) (angle 0.0))'.format(spacing, y_max))
@@ -267,9 +294,7 @@ def generate_sym(
                 y = get_y(j, i, spacing)
                 dx = spacing / 8 * 1.5
                 dy = spacing / 8 / 1.5
-                lines.append(' (polygon {} (layer sym_outlines)'.format(
-                    uuid(kind, 'polygon', i, 'sym_decoration')
-                ))
+                lines.append(' (polygon {} (layer sym_outlines)'.format(uuid_decoration))
                 lines.append('  (width {}) (fill true) (grab true)'.format(line_width))
                 vertex = '  (vertex (pos {} {}) (angle 0.0))'
                 lines.append(vertex.format(spacing / 2 - dx, y + dy))
@@ -284,9 +309,7 @@ def generate_sym(
                 y = get_y(j, i, spacing)
                 d = spacing / 4 * 0.75
                 w = line_width * 0.75
-                lines.append(' (polygon {} (layer sym_outlines)'.format(
-                    uuid(kind, 'polygon', i, 'sym_decoration')
-                ))
+                lines.append(' (polygon {} (layer sym_outlines)'.format(uuid_decoration))
                 lines.append('  (width {}) (fill false) (grab false) '.format(w))
                 lines.append('  (vertex (pos {} {}) (angle 135.0))'.format(
                     spacing / 2 + d * 0.5 - d - w, y - d),
@@ -298,16 +321,12 @@ def generate_sym(
 
         # Text
         y_max, y_min = get_rectangle_bounds(i, spacing, spacing)
-        lines.append(' (text {} (layer sym_names) (value "{{{{NAME}}}}")'.format(
-            uuid(kind, 'text-name', i)),
-        )
+        lines.append(' (text {} (layer sym_names) (value "{{{{NAME}}}}")'.format(uuid_text_name))
         lines.append('  (align center bottom) (height {}) (position 0.0 {}) (rotation 0.0)'.format(
             sym_text_height, y_max,
         ))
         lines.append(' )')
-        lines.append(' (text {} (layer sym_names) (value "{{{{VALUE}}}}")'.format(
-            uuid(kind, 'text-value', i)),
-        )
+        lines.append(' (text {} (layer sym_names) (value "{{{{VALUE}}}}")'.format(uuid_text_value))
         lines.append('  (align center top) (height {}) (position 0.0 {}) (rotation 0.0)'.format(
             sym_text_height, y_min,
         ))
@@ -315,7 +334,7 @@ def generate_sym(
 
         lines.append(')')
 
-        sym_dir_path = path.join(dirpath, sym_uuid)
+        sym_dir_path = path.join(dirpath, uuid_sym)
         if not (path.exists(sym_dir_path) and path.isdir(sym_dir_path)):
             makedirs(sym_dir_path)
         with open(path.join(sym_dir_path, '.librepcb-sym'), 'w') as f:
@@ -324,7 +343,7 @@ def generate_sym(
             f.write('\n'.join(lines))
             f.write('\n')
 
-        print('1x{}: Wrote symbol {}'.format(i, sym_uuid))
+        print('1x{}: Wrote symbol {}'.format(i, uuid_sym))
 
 
 def generate_cmp(
@@ -337,13 +356,24 @@ def generate_cmp(
     min_pads: int,
     max_pads: int,
 ):
+    category = 'cmp'
     for i in range(min_pads, max_pads + 1):
         lines = []
 
-        cmp_uuid = uuid(kind, 'cmp', i)
+        variant = '1x{}'.format(i)
+
+        def _uuid(identifier):
+            return uuid(category, kind, variant, identifier)
+
+        uuid_cmp = _uuid('cmp')
+        uuid_pins = [uuid('sym', kind, variant, 'pin-{}'.format(p)) for p in range(i)]
+        uuid_signals = [_uuid('signal-{}'.format(p)) for p in range(i)]
+        uuid_variant = _uuid('variant-default')
+        uuid_gate = _uuid('gate-default')
+        uuid_symbol = uuid('sym', kind, variant, 'sym')
 
         # General info
-        lines.append('(librepcb_component {}'.format(cmp_uuid))
+        lines.append('(librepcb_component {}'.format(uuid_cmp))
         lines.append(' (name "{} 1x{}")'.format(name, i))
         lines.append(' (description "A 1x{} {}.\\n\\n'
                      'Generated with {}")'.format(i, name_lower, generator))
@@ -357,32 +387,26 @@ def generate_cmp(
         lines.append(' (default_value "")')
         lines.append(' (prefix "J")')
 
-        pin_uuids = [uuid(kind, 'pin', i, str(p)) for p in range(i)]
-        signal_uuids = [uuid(kind, 'signal', i, str(p)) for p in range(i)]
-        variant_uuid = uuid(kind, 'cmp-variant', i)
-        gate_uuid = uuid(kind, 'cmp-gate', i)
-        symbol_uuid = uuid(kind, 'sym', i)
-
         for j in range(1, i + 1):
-            lines.append(' (signal {} (name "{}") (role passive)'.format(signal_uuids[j - 1], j))
+            lines.append(' (signal {} (name "{}") (role passive)'.format(uuid_signals[j - 1], j))
             lines.append('  (required false) (negated false) (clock false) (forced_net "")')
             lines.append(' )')
-        lines.append(' (variant {} (norm "")'.format(variant_uuid))
+        lines.append(' (variant {} (norm "")'.format(uuid_variant))
         lines.append('  (name "default")')
         lines.append('  (description "")')
-        lines.append('  (gate {}'.format(gate_uuid))
-        lines.append('   (symbol {})'.format(symbol_uuid))
+        lines.append('  (gate {}'.format(uuid_gate))
+        lines.append('   (symbol {})'.format(uuid_symbol))
         lines.append('   (position 0.0 0.0) (rotation 0.0) (required true) (suffix "")')
         for j in range(1, i + 1):
             lines.append('   (pin {} (signal {}) (text pin))'.format(
-                pin_uuids[j - 1],
-                signal_uuids[j - 1],
+                uuid_pins[j - 1],
+                uuid_signals[j - 1],
             ))
         lines.append('  )')
         lines.append(' )')
         lines.append(')')
 
-        cmp_dir_path = path.join(dirpath, cmp_uuid)
+        cmp_dir_path = path.join(dirpath, uuid_cmp)
         if not (path.exists(cmp_dir_path) and path.isdir(cmp_dir_path)):
             makedirs(cmp_dir_path)
         with open(path.join(cmp_dir_path, '.librepcb-cmp'), 'w') as f:
@@ -391,7 +415,7 @@ def generate_cmp(
             f.write('\n'.join(lines))
             f.write('\n')
 
-        print('1x{}: Wrote component {}'.format(i, cmp_uuid))
+        print('1x{}: Wrote component {}'.format(i, uuid_cmp))
 
 
 if __name__ == '__main__':
