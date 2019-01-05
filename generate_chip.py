0a8ac96ce15372bc1e0bffd3eba001ad4ff7b45d
Danilo Bargen <mail@dbrgn.ch>
2019-01-05 12:55:07 +0100

    generate_chip: Remove height from generic parts

--- generate_chip.py
+++ generate_chip.py
@@ -281,9 +281,9 @@ if __name__ == '__main__':
     generate_pkg(
         dirpath='out/chip/pkg',
         author='Danilo B.',
-        name='RESC{size_metric}X{height} ({size_imperial})',
-        description='Chip resistor {size_metric} (imperial {size_imperial}).\\n\\n'
-                    'Length: {length}mm\\nWidth: {width}mm\\nHeight: max {height}mm',
+        name='RESC{size_metric} ({size_imperial})',
+        description='Generic chip resistor {size_metric} (imperial {size_imperial}).\\n\\n'
+                    'Length: {length}mm\\nWidth: {width}mm',
         configs=[
             # Configuration: Values taken from Samsung specs.
             #        imperial, len, wid,  hght, gap
@@ -299,23 +299,22 @@ if __name__ == '__main__':
             ChipConfig('2512', 6.4, 3.2,  0.70, 4.6),   # noqa
         ],
         pkgcat='a20f0330-06d3-4bc2-a1fa-f8577deb6770',
-        keywords='r,resistor,chip',
+        keywords='r,resistor,chip,generic',
         create_date='2018-12-19T00:08:03Z',
     )
     # J-Lead resistors (RESJ)
     generate_pkg(
         dirpath='out/chip/pkg',
         author='Danilo B.',
-        name='RESJ{size_metric}X{height} ({size_imperial})',
-        description='J-lead resistor {size_metric} (imperial {size_imperial}).\\n\\n'
-                    'Length: {length}mm\\nWidth: {width}mm\\nHeight: max {height}mm',
+        name='RESJ{size_metric} ({size_imperial})',
+        description='Generic J-lead resistor {size_metric} (imperial {size_imperial}).\\n\\n'
+                    'Length: {length}mm\\nWidth: {width}mm',
         configs=[
             #        imperial, len,   wid,  hght, gap
-            ChipConfig('4527', 11.56, 6.98, 2.54, 5.2),  # noqa
-            ChipConfig('4527', 11.56, 6.98, 5.84, 5.2),  # noqa
+            ChipConfig('4527', 11.56, 6.98, 5.84, 5.2),
         ],
         pkgcat='a20f0330-06d3-4bc2-a1fa-f8577deb6770',
-        keywords='r,resistor,j-lead',
+        keywords='r,resistor,j-lead,generic',
         create_date='2019-01-04T23:06:17Z',
     )
     save_cache(uuid_cache_file, uuid_cache)
