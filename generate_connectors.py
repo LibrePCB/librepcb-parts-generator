cdf6ea1a40def4d42c0787ed1c453558b99f951c
Raphael Nestler <raphael.nestler@gmail.com>
2019-06-16 03:41:28 +0200

    Bump connector symbol versions

--- generate_connectors.py
+++ generate_connectors.py
@@ -255,6 +255,7 @@ def generate_sym(
     keywords: str,
     min_pads: int,
     max_pads: int,
+    version: str,
     create_date: Optional[str],
 ):
     category = 'sym'
@@ -280,7 +281,7 @@ def generate_sym(
                      'Generated with {}")'.format(i, name_lower, generator))
         lines.append(' (keywords "connector, 1x{}, {}")'.format(i, keywords))
         lines.append(' (author "{}")'.format(author))
-        lines.append(' (version "0.1")')
+        lines.append(' (version "{}")'.format(version))
         lines.append(' (created {})'.format(create_date or now()))
         lines.append(' (deprecated false)')
         lines.append(' (category {})'.format(cmpcat))
@@ -517,6 +518,7 @@ if __name__ == '__main__':
         keywords='pin header, male header',
         min_pads=1,
         max_pads=40,
+        version='0.2',
         create_date='2018-10-17T19:13:41Z',
     )
     generate_sym(
@@ -529,6 +531,7 @@ if __name__ == '__main__':
         keywords='pin socket, female header',
         min_pads=1,
         max_pads=40,
+        version='0.2',
         create_date='2018-10-17T19:13:41Z',
     )
     generate_sym(
@@ -541,6 +544,7 @@ if __name__ == '__main__':
         keywords='connector, generic',
         min_pads=1,
         max_pads=40,
+        version='0.2',
         create_date='2018-10-17T19:13:41Z',
     )
     generate_cmp(
