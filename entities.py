7c6ae98484d671abddd9d5ce414f798102083042
Raphael Nestler <raphael.nestler@gmail.com>
2019-07-09 11:00:50 +0200

    Add more entities

--- entities.py
+++ entities.py
@@ -2,6 +2,99 @@
 LibrePCB S-expression entities
 """
 
+from common import format_float
+from typing import List
+
+
+class DateValue(object):
+    """Helper class to represent a single named date value"""
+    def __init__(self, name: str, date: str):
+        self.name = name
+        self.date = date
+
+    def __str__(self) -> str:
+        return '({} {})'.format(self.name, self.date)
+
+
+class UUIDValue(object):
+    """Helper class to represent a single named UUID value"""
+    def __init__(self, name: str, uuid: str):
+        self.name = name
+        self.uuid = uuid
+
+    def __str__(self) -> str:
+        return '({} {})'.format(self.name, self.uuid)
+
+
+class BoolValue(object):
+    """Helper class to represent a single named boolean value"""
+    def __init__(self, name: str, value: bool):
+        self.name = name
+        self.value = str(value).lower()
+
+    def __str__(self) -> str:
+        return '({} {})'.format(self.name, self.value)
+
+
+class StringValue(object):
+    """Helper class to represent a single named string value"""
+    def __init__(self, name: str, value: str):
+        self.name = name
+        self.value = value
+
+    def __str__(self) -> str:
+        return '({} "{}")'.format(self.name, self.value)
+
+
+class FloatValue(object):
+    """Helper class to represent a single named float value"""
+    def __init__(self, name: str, value: float):
+        self.name = name
+        self.value = value
+
+    def __str__(self) -> str:
+        return '({} {})'.format(self.name, format_float(self.value))
+
+
+class Name(StringValue):
+    def __init__(self, name: str):
+        super().__init__('name', name)
+
+
+class Description(StringValue):
+    def __init__(self, description: str):
+        super().__init__('description', description)
+
+
+class Keywords(StringValue):
+    def __init__(self, keywords: str):
+        super().__init__('keywords', keywords)
+
+
+class Author(StringValue):
+    def __init__(self, author: str):
+        super().__init__('author', author)
+
+
+class Version(StringValue):
+    def __init__(self, version: str):
+        super().__init__('version', version)
+
+
+class Created(DateValue):
+    def __init__(self, created: str):
+        super().__init__('created', created)
+
+
+class Deprecated(BoolValue):
+    def __init__(self, deprecated: bool):
+        super().__init__('deprecated', deprecated)
+
+
+class Category(UUIDValue):
+    def __init__(self, category: str):
+        super().__init__('category', category)
+
 
 class Position(object):
     def __init__(self, x: float, y: float):
@@ -12,24 +105,18 @@ class Position(object):
         return '(position {} {})'.format(self.x, self.y)
 
 
-class Rotation(object):
+class Rotation(FloatValue):
     def __init__(self, rotation: float):
-        self.rotation = rotation
-
-    def __str__(self) -> str:
-        return '(rotation {})'.format(self.rotation)
+        super().__init__('rotation', rotation)
 
 
-class Length(object):
+class Length(FloatValue):
     def __init__(self, length: float):
-        self.length = length
-
-    def __str__(self) -> str:
-        return '(length {})'.format(self.length)
+        super().__init__('length', length)
 
 
 class SchematicsPin(object):
-    def __init__(self, uuid: str, name: str, position: Position, rotation: Rotation, length: Length):
+    def __init__(self, uuid: str, name: Name, position: Position, rotation: Rotation, length: Length):
         self.uuid = uuid
         self.name = name
         self.position = position
@@ -37,6 +124,98 @@ class SchematicsPin(object):
         self.length = length
 
     def __str__(self) -> str:
-        return '(pin {} (name "{}")\n'.format(self.uuid, self.name) +\
+        return '(pin {} {}\n'.format(self.uuid, self.name) +\
             ' {} {} {}\n'.format(self.position, self.rotation, self.length) +\
             ')'
+
+
+class Width(FloatValue):
+    def __init__(self, width: float):
+        super().__init__('width', width)
+
+
+class Height(FloatValue):
+    def __init__(self, height: float):
+        super().__init__('height', height)
+
+
+class Angle(FloatValue):
+    def __init__(self, angle: float):
+        super().__init__('angle', angle)
+
+
+class Fill(BoolValue):
+    def __init__(self, fill: bool):
+        super().__init__('fill', fill)
+
+
+class GrabArea(BoolValue):
+    def __init__(self, grab_area: bool):
+        super().__init__('grab_area', grab_area)
+
+
+class Vertex(object):
+    def __init__(self, position: Position, angle: Angle):
+        self.position = position
+        self.angle = angle
+
+    def __str__(self) -> str:
+        return '(vertex {} {})'.format(self.position, self.angle)
+
+
+class Layer(object):
+    def __init__(self, layer: str):
+        self.layer = layer
+
+    def __str__(self) -> str:
+        return '(layer {})'.format(self.layer)
+
+
+class Polygon(object):
+    def __init__(self, uuid: str, layer: Layer, width: Width, fill: Fill, grab_area: GrabArea):
+        self.uuid = uuid
+        self.layer = layer
+        self.width = width
+        self.fill = fill
+        self.grab_area = grab_area
+        self.vertices = []  # type: List[Vertex]
+
+    def add_vertex(self, vertex: Vertex) -> None:
+        self.vertices.append(vertex)
+
+    def __str__(self) -> str:
+        ret = '(polygon {} {}\n'.format(self.uuid, self.layer) +\
+            ' {} {} {}\n'.format(self.width, self.fill, self.grab_area)
+        for vertex in self.vertices:
+            ret += ' {}\n'.format(vertex)
+        ret += ')'
+        return ret
+
+
+class Value(StringValue):
+    def __init__(self, value: str):
+        super().__init__('value', value)
+
+
+class Align(object):
+    def __init__(self, align: str):
+        self.align = align
+
+    def __str__(self) -> str:
+        return '(align {})'.format(self.align)
+
+
+class Text(object):
+    def __init__(self, uuid: str, layer: Layer, value: Value, align: Align, height: Height, position: Position, rotation: Rotation):
+        self.uuid = uuid
+        self.layer = layer
+        self.value = value
+        self.align = align
+        self.height = height
+        self.position = position
+        self.rotation = rotation
+
+    def __str__(self) -> str:
+        return '(text {} {} {}\n'.format(self.uuid, self.layer, self.value) +\
+               ' {} {} {} {}\n'.format(self.align, self.height, self.position, self.rotation) +\
+               ')'
--- test_entities.py
+++ test_entities.py
@@ -1,4 +1,14 @@
-from entities import Position, Rotation, Length, SchematicsPin
+from entities import Name, Description, Position, Rotation, Length, SchematicsPin, Vertex, Angle, Polygon, Width, Fill, GrabArea, Layer, Align, Height, Text, Value
+
+
+def test_name():
+    name_s_exp = str(Name("bar"))
+    assert name_s_exp == '(name "bar")'
+
+
+def test_description():
+    description = str(Description("My Description\\nWith two lines"))
+    assert description == '(description "My Description\\nWith two lines")'
 
 
 def test_position():
@@ -17,7 +27,40 @@ def test_length():
 
 
 def test_schematics_pin():
-    schematics_pin_s_exp = str(SchematicsPin('my_uuid', 'foo', Position(1.0, 2.0), Rotation(180.0), Length(3.81)))
+    schematics_pin_s_exp = str(SchematicsPin('my_uuid', Name('foo'), Position(1.0, 2.0), Rotation(180.0), Length(3.81)))
+
     assert schematics_pin_s_exp == '(pin my_uuid (name "foo")\n' + \
         ' (position 1.0 2.0) (rotation 180.0) (length 3.81)\n' + \
+        ')'
+
+
+def test_vertex():
+    vertex = str(Vertex(Position(-2.54, 22.86), Angle(0.0)))
+    assert vertex == '(vertex (position -2.54 22.86) (angle 0.0))'
+
+
+def test_polygon():
+    polygon = Polygon('743dbf3d-98e8-46f0-9a32-00e00d0e811f', Layer('sym_outlines'), Width(0.25), Fill(False), GrabArea(True))
+    polygon.add_vertex(Vertex(Position(-2.54, 22.86), Angle(0.0)))
+    polygon.add_vertex(Vertex(Position( 2.54, 22.86), Angle(0.0)))
+    polygon.add_vertex(Vertex(Position( 2.54, -25.4), Angle(0.0)))
+    polygon.add_vertex(Vertex(Position(-2.54, -25.4), Angle(0.0)))
+    polygon.add_vertex(Vertex(Position(-2.54, 22.86), Angle(0.0)))
+
+    assert str(polygon) == '(polygon 743dbf3d-98e8-46f0-9a32-00e00d0e811f (layer sym_outlines)\n' +\
+        ' (width 0.25) (fill false) (grab_area true)\n' +\
+        ' (vertex (position -2.54 22.86) (angle 0.0))\n' +\
+        ' (vertex (position 2.54 22.86) (angle 0.0))\n' +\
+        ' (vertex (position 2.54 -25.4) (angle 0.0))\n' +\
+        ' (vertex (position -2.54 -25.4) (angle 0.0))\n' +\
+        ' (vertex (position -2.54 22.86) (angle 0.0))\n' +\
+        ')'
+
+
+def test_text():
+    text = str(Text('b9c4aa19-0a46-400c-9c96-e8c3dfb8f83e', Layer('sym_names'),
+                    Value('{{NAME}}'), Align('center bottom'), Height(2.54),
+                    Position(0.0, 22.86), Rotation(0.0)))
+    assert text == '(text b9c4aa19-0a46-400c-9c96-e8c3dfb8f83e (layer sym_names) (value "{{NAME}}")\n' +\
+        ' (align center bottom) (height 2.54) (position 0.0 22.86) (rotation 0.0)\n' +\
         ')'
