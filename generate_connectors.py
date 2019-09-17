c3f4d954e59edc1014a7160713ea8bfd51700c51
Danilo Bargen <mail@dbrgn.ch>
2019-09-17 22:04:06 +0200

    connectors: Add 2-row pin header / socket packages

--- generate_connectors.py
+++ generate_connectors.py
@@ -34,7 +34,7 @@ generator = 'librepcb-parts-generator (generate_connectors.py)'
 
 width = 2.54
 spacing = 2.54
-pad_size = (2.54, 1.27 * 1.25)
+pad_size = (2.54 - 0.35, 1.27 * 1.25)
 line_width = 0.25
 pkg_text_height = 1.0
 sym_text_height = 2.54
@@ -121,19 +121,24 @@ def generate_pkg(
     kind: str,
     pkgcat: str,
     keywords: str,
+    rows: int,
     min_pads: int,
     max_pads: int,
-    top_offset: float,
     pad_drills: Iterable[float],
-    generate_silkscreen: Callable[[List[str], str, str, str, int, float], None],
+    generate_silkscreen: Callable[[List[str], str, str, str, int, int], None],
+    version: str,
     create_date: Optional[str],
 ) -> None:
     category = 'pkg'
-    for i in range(min_pads, max_pads + 1):
+    assert rows in [1, 2]
+    for i in range(min_pads, max_pads + 1, rows):
         for drill in pad_drills:
-            lines = []
+            per_row = i // rows
+            top_offset = spacing / 2
 
-            variant = '1x{}-D{:.1f}'.format(i, drill)
+            variant = '{}x{}-D{:.1f}'.format(rows, per_row, drill)
+
+            lines = []
 
             def _uuid(identifier: str) -> str:
                 return uuid(category, kind, variant, identifier)
@@ -146,13 +151,13 @@ def generate_pkg(
 
             # General info
             lines.append('(librepcb_package {}'.format(uuid_pkg))
-            lines.append(' (name "{} 1x{} ⌀{:.1f}mm")'.format(name, i, drill))
-            lines.append(' (description "A 1x{} {} with {}mm pin spacing '
+            lines.append(' (name "{} {}x{:02d} ⌀{:.1f}mm")'.format(name, rows, per_row, drill))
+            lines.append(' (description "A {}x{} {} with {}mm pin spacing '
                          'and {:.1f}mm drill holes.\\n\\n'
-                         'Generated with {}")'.format(i, name_lower, spacing, drill, generator))
-            lines.append(' (keywords "connector, 1x{}, d{:.1f}, {}")'.format(i, drill, keywords))
+                         'Generated with {}")'.format(rows, per_row, name_lower, spacing, drill, generator))
+            lines.append(' (keywords "connector, {}x{}, d{:.1f}, {}")'.format(rows, per_row, drill, keywords))
             lines.append(' (author "{}")'.format(author))
-            lines.append(' (version "0.1")')
+            lines.append(' (version "{}")'.format(version))
             lines.append(' (created {})'.format(create_date or now()))
             lines.append(' (deprecated false)')
             lines.append(' (category {})'.format(pkgcat))
@@ -163,20 +168,24 @@ def generate_pkg(
             lines.append('  (description "")')
 
             # Pads
-            for j in range(1, i + 1):
-                y = get_y(j, i, 1, spacing, False)
-                shape = 'rect' if j == 1 else 'round'
-                lines.append('  (pad {} (side tht) (shape {})'.format(uuid_pads[j - 1], shape))
-                lines.append('   (position 0.0 {}) (rotation 0.0) (size {} {}) (drill {})'.format(
-                    y, pad_size[0], pad_size[1], drill,
+            for p in range(1, i + 1):
+                if rows == 1:
+                    x = 0.0
+                elif rows == 2:
+                    x = spacing / 2 if (p % rows == 0) else -spacing / 2
+                y = get_y(p, i, rows, spacing, False)
+                shape = 'rect' if p == 1 else 'round'
+                lines.append('  (pad {} (side tht) (shape {})'.format(uuid_pads[p - 1], shape))
+                lines.append('   (position {} {}) (rotation 0.0) (size {} {}) (drill {})'.format(
+                    ff(x), ff(y), ff(pad_size[0]), ff(pad_size[1]), drill,
                 ))
                 lines.append('  )')
 
             # Silkscreen
-            generate_silkscreen(lines, category, kind, variant, i, top_offset)
+            generate_silkscreen(lines, category, kind, variant, i, rows)
 
             # Labels
-            y_max, y_min = get_rectangle_bounds(i, 1, spacing, top_offset + 1.27, False)
+            y_max, y_min = get_rectangle_bounds(i, rows, spacing, top_offset + 1.27, False)
             text_attrs = '(height {}) (stroke_width 0.2) ' \
                          '(letter_spacing auto) (line_spacing auto)'.format(pkg_text_height)
             lines.append('  (stroke_text {} (layer top_names)'.format(uuid_text_name))
@@ -206,7 +215,7 @@ def generate_pkg(
                 f.write('\n'.join(lines))
                 f.write('\n')
 
-            print('1x{} {} ⌀{:.1f}mm: Wrote package {}'.format(i, kind, drill, uuid_pkg))
+            print('{}x{:02d} {} ⌀{:.1f}mm: Wrote package {}'.format(rows, per_row, kind, drill, uuid_pkg))
 
 
 def generate_silkscreen_female(
@@ -215,18 +224,21 @@ def generate_silkscreen_female(
     kind: str,
     variant: str,
     pin_count: int,
-    top_offset: float,
+    rows: int,
 ) -> None:
     uuid_polygon = uuid(category, kind, variant, 'polygon-contour')
 
+    x = 1.27 * rows + line_width / 2
+    top_offset = spacing / 2 + line_width / 2
+
     lines.append('  (polygon {} (layer top_placement)'.format(uuid_polygon))
     lines.append('   (width {}) (fill false) (grab_area true)'.format(line_width))
-    y_max, y_min = get_rectangle_bounds(pin_count, 1, spacing, top_offset, False)
-    lines.append('   (vertex (position -1.27 {}) (angle 0.0))'.format(ff(y_max)))
-    lines.append('   (vertex (position 1.27 {}) (angle 0.0))'.format(ff(y_max)))
-    lines.append('   (vertex (position 1.27 {}) (angle 0.0))'.format(ff(y_min)))
-    lines.append('   (vertex (position -1.27 {}) (angle 0.0))'.format(ff(y_min)))
-    lines.append('   (vertex (position -1.27 {}) (angle 0.0))'.format(ff(y_max)))
+    y_max, y_min = get_rectangle_bounds(pin_count, rows, spacing, top_offset, False)
+    lines.append('   (vertex (position -{} {}) (angle 0.0))'.format(ff(x), ff(y_max)))
+    lines.append('   (vertex (position {} {}) (angle 0.0))'.format(ff(x), ff(y_max)))
+    lines.append('   (vertex (position {} {}) (angle 0.0))'.format(ff(x), ff(y_min)))
+    lines.append('   (vertex (position -{} {}) (angle 0.0))'.format(ff(x), ff(y_min)))
+    lines.append('   (vertex (position -{} {}) (angle 0.0))'.format(ff(x), ff(y_max)))
     lines.append('  )')
 
 
@@ -236,30 +248,39 @@ def generate_silkscreen_male(
     kind: str,
     variant: str,
     pin_count: int,
-    top_offset: float,
+    rows: int,
 ) -> None:
     uuid_polygon = uuid(category, kind, variant, 'polygon-contour')
+
+    per_row = pin_count // rows
+    x_outer = 1.27 * rows + line_width / 2
+    x_inner = x_outer - 0.27
+    offset = line_width / 2
 
     # Start in top right corner, go around the pads clockwise
     lines.append('  (polygon {} (layer top_placement)'.format(uuid_polygon))
     lines.append('   (width {}) (fill false) (grab_area true)'.format(line_width))
     # Down on the right
-    for pin in range(1, pin_count + 1):
-        y = get_y(pin, pin_count, 1, spacing, False)
-        lines.append('   (vertex (position 1.27 {}) (angle 0.0))'.format(ff(y + 1)))
-        lines.append('   (vertex (position 1.27 {}) (angle 0.0))'.format(ff(y - 1)))
-        lines.append('   (vertex (position 1.0 {}) (angle 0.0))'.format(ff(y - 1.27)))
+    for pin in range(1, per_row + 1):
+        y = get_y(pin, per_row, 1, spacing, False)
+        top_offset = offset if pin == 1 else 0
+        bot_offset = offset if pin == per_row else 0
+        lines.append('   (vertex (position {} {}) (angle 0.0))'.format(ff(x_outer), ff(y + 1 + top_offset)))
+        lines.append('   (vertex (position {} {}) (angle 0.0))'.format(ff(x_outer), ff(y - 1 - bot_offset)))
+        lines.append('   (vertex (position {} {}) (angle 0.0))'.format(ff(x_inner), ff(y - 1.27 - bot_offset)))
     # Up on the left
-    for pin in range(pin_count, 0, -1):
-        y = get_y(pin, pin_count, 1, spacing, False)
-        lines.append('   (vertex (position -1.0 {}) (angle 0.0))'.format(ff(y - 1.27)))
-        lines.append('   (vertex (position -1.27 {}) (angle 0.0))'.format(ff(y - 1)))
-        lines.append('   (vertex (position -1.27 {}) (angle 0.0))'.format(ff(y + 1)))
+    for pin in range(per_row, 0, -1):
+        y = get_y(pin, per_row, 1, spacing, False)
+        top_offset = offset if pin == 1 else 0
+        bot_offset = offset if pin == per_row else 0
+        lines.append('   (vertex (position -{} {}) (angle 0.0))'.format(ff(x_inner), ff(y - 1.27 - bot_offset)))
+        lines.append('   (vertex (position -{} {}) (angle 0.0))'.format(ff(x_outer), ff(y - 1 - bot_offset)))
+        lines.append('   (vertex (position -{} {}) (angle 0.0))'.format(ff(x_outer), ff(y + 1 + top_offset)))
     # Back to start
-    top_y = get_y(1, pin_count, 1, spacing, False) + spacing / 2
-    lines.append('   (vertex (position -1.0 {}) (angle 0.0))'.format(ff(top_y)))
-    lines.append('   (vertex (position 1.0 {}) (angle 0.0))'.format(ff(top_y)))
-    lines.append('   (vertex (position 1.27 {}) (angle 0.0))'.format(ff(top_y - 0.27)))
+    top_y = get_y(1, per_row, 1, spacing, False) + spacing / 2 + offset
+    lines.append('   (vertex (position -{} {}) (angle 0.0))'.format(ff(x_inner), ff(top_y)))
+    lines.append('   (vertex (position {} {}) (angle 0.0))'.format(ff(x_inner), ff(top_y)))
+    lines.append('   (vertex (position {} {}) (angle 0.0))'.format(ff(x_outer), ff(top_y - 0.27)))
     lines.append('  )')
 
 
@@ -298,7 +319,7 @@ def generate_sym(
         # General info
         symbol = Symbol(
             uuid_sym,
-            Name('{} {}x{}'.format(name, rows, per_row)),
+            Name('{} {}x{:02d}'.format(name, rows, per_row)),
             Description('A {}x{} {}.\\n\\n'
                         'Generated with {}'.format(rows, per_row, name_lower, generator)),
             Keywords('connector, {}x{}, {}'.format(rows, per_row, keywords)),
@@ -429,7 +450,7 @@ def generate_cmp(
         # General info
         component = Component(
             uuid_cmp,
-            Name('{} {}x{}'.format(name, rows, per_row)),
+            Name('{} {}x{:02d}'.format(name, rows, per_row)),
             Description('A {}x{} {}.\\n\\n'
                         'Generated with {}'.format(rows, per_row, name_lower, generator)),
             Keywords('connector, {}x{}, {}'.format(rows, per_row, keywords)),
@@ -473,7 +494,7 @@ def generate_cmp(
 
         component.serialize(dirpath)
 
-        print('{}x{} {}: Wrote component {}'.format(rows, i, kind, uuid_cmp))
+        print('{}x{} {}: Wrote component {}'.format(rows, per_row, kind, uuid_cmp))
 
 
 def generate_dev(
@@ -612,12 +633,29 @@ if __name__ == '__main__':
         kind=KIND_HEADER,
         pkgcat='e4d3a6bf-af32-48a2-b427-5e794bed949a',
         keywords='pin header, male header, tht',
+        rows=1,
         min_pads=1,
         max_pads=40,
-        top_offset=1.27,
         pad_drills=[0.9, 1.0, 1.1],
         generate_silkscreen=generate_silkscreen_male,
+        version='0.2',
         create_date='2018-10-17T19:13:41Z',
+    )
+    generate_pkg(
+        dirpath='out/connectors/pkg',
+        author='Danilo B.',
+        name='Pin Header 2.54mm',
+        name_lower='male pin header',
+        kind=KIND_HEADER,
+        pkgcat='e4d3a6bf-af32-48a2-b427-5e794bed949a',
+        keywords='pin header, male header, tht',
+        rows=2,
+        min_pads=4,
+        max_pads=80,
+        pad_drills=[0.9, 1.0, 1.1],
+        generate_silkscreen=generate_silkscreen_male,
+        version='0.2',
+        create_date='2019-09-17T20:00:41Z',
     )
     generate_dev(
         dirpath='out/connectors/dev',
@@ -700,12 +738,29 @@ if __name__ == '__main__':
         kind=KIND_SOCKET,
         pkgcat='6183d171-e810-475a-a568-2a270aff8f5e',
         keywords='pin socket, female header, tht',
+        rows=1,
         min_pads=1,
         max_pads=40,
-        top_offset=1.5,
         pad_drills=[0.9, 1.0, 1.1],
         generate_silkscreen=generate_silkscreen_female,
+        version='0.2',
         create_date='2018-10-17T19:13:41Z',
+    )
+    generate_pkg(
+        dirpath='out/connectors/pkg',
+        author='Danilo B.',
+        name='Pin Socket 2.54mm',
+        name_lower='female pin socket',
+        kind=KIND_SOCKET,
+        pkgcat='6183d171-e810-475a-a568-2a270aff8f5e',
+        keywords='pin socket, female header, tht',
+        rows=2,
+        min_pads=4,
+        max_pads=80,
+        pad_drills=[0.9, 1.0, 1.1],
+        generate_silkscreen=generate_silkscreen_female,
+        version='0.2',
+        create_date='2019-09-17T20:00:41Z',
     )
     generate_dev(
         dirpath='out/connectors/dev',
@@ -761,11 +816,12 @@ if __name__ == '__main__':
         kind=KIND_WIRE_CONNECTOR,
         pkgcat='56a5773f-eeb4-4b39-8cb9-274f3da26f4f',
         keywords='connector, soldering, generic',
+        rows=1,
         min_pads=1,
         max_pads=40,
-        top_offset=1.5,
         pad_drills=[1.0],
         generate_silkscreen=generate_silkscreen_female,
+        version='0.2',
         create_date='2018-10-17T19:13:41Z',
     )
     generate_dev(
