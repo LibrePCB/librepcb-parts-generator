c1f74aee19e5f7f56ddeff880c65b776a9e9330c
ouabache <z3qmtr45@gmail.com>
2020-01-31 17:13:47 -0800

    updated scripts

--- STM32/split_base.pl
+++ STM32/split_base.pl
@@ -1,6 +1,7 @@
 # importing csv module
 import csv
 import argparse
+import sys
 
 
 parser = argparse.ArgumentParser(description='create a symbol from csv file')
@@ -22,17 +23,29 @@ boilerplate_raw_data = []
 
 """
 """
+from os import makedirs, path
+    
+if __name__ == '__main__':
+    def _make(dirpath: str) -> None:
+        if not (path.exists(dirpath) and path.isdir(dirpath)):
+            makedirs(dirpath)
+    _make('topsrc')
+    _make('pinsrc')
 
+base_cvs_file = 'Base.csv'
 
-boilerplate_cvs_file = 'Base.csv'
-
-with open(boilerplate_cvs_file, 'r') as CSVBfile:
+with open(base_cvs_file, 'r') as CSVBfile:
 
     CSVbreader = csv.reader(CSVBfile)
     for brow in CSVbreader:
         boilerplate_raw_data.append(brow)
     num_of_brows = CSVbreader.line_num
 
+
+script = open("script.txt", "w")
+run_generate = open("run_generate", "w")
+script.write("rm -r out\n")
+    
 line_number = 1
 for brow in boilerplate_raw_data[:num_of_brows]:
     # parsing each column of a row
@@ -40,8 +53,9 @@ for brow in boilerplate_raw_data[:num_of_brows]:
         if brow[2] == "Active":
             design = brow[1]
             footprint = brow[5]
-            print("load  {}".format(design))
-            print("csv pinout pinsrc/{}.csv".format(design))
+            script.write("load  {}\n".format(design))
+            script.write("csv pinout pinsrc/{}.csv\n".format(design))
+            run_generate.write("python3.7 ../generate_stm.py  --group stm32   --directory  ./topsrc/    --design {}\n".format(design))
             filename = "topsrc/{}.csv".format(design)
             file = open(filename, "w")
             file.write("DEF,{}\n".format(design))
@@ -51,4 +65,6 @@ for brow in boilerplate_raw_data[:num_of_brows]:
 
     line_number = line_number + 1
 
-print("exit")
+script.write("exit\n")
+script.close()
+run_generate.close()	     
